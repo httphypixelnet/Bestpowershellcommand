1697525a01dbb70ab8078a3578eba2630b9eb042
StubblyCamera51 <74626731+StubblyCamera51@users.noreply.github.com>
2022-09-29 08:09:55 -0700

    Update Launch.ps1

--- Launch.ps1
+++ Launch.ps1
@@ -6,9 +6,7 @@ if (Test-Path -Path "C:\Program Files (x86)\PaperCut MF Client\runtime\jre\bin\j
     $javaPath = "C:\Program Files (x86)\PaperCut MF Client\runtime\jre\bin\java.exe"
 }
 
-if (Test-Path -Path $path) {
-    $wshell.Popup("MC-Launcher.jar is already installed! Click ok to launch MC-Launcher.jar")
-} else {
+if (-not(Test-Path -Path $path)) {
     Invoke-WebRequest -Uri "https://tlaun.ch/dl/mcl/jar" -OutFile $path
     $wshell.Popup("Successfully installed MC-Launcher.jar! Click ok to launch MC-Launcher.jar")
 }
